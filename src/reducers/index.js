3b16bbdf8a2079a84865ea3eddba42e037a00227
realstevelee <realstevelee@gmail.com>
2020-12-19 14:20:17 -0500

    first commit

---
+++ src/reducers/index.js
@@ -0,0 +1,23 @@
+import { combineReducers } from 'redux'
+
+const songsReducer = () => {
+    return [
+        { title: 'All I want for Chrsitmas is you', duration: '3:58' },
+        { title: 'Dynamite', duration: '3:36' },
+        { title: 'Position', duration: '2:52' },
+        { title: 'Blinding Lights', duration: '3:20' }
+    ]
+}
+
+const selectedSongReducer = (selectedSong = null, action) => {
+    if (action.type === 'SONG_SELECTED') {
+        return action.payload
+    }
+
+    return selectedSong
+}
+
+export default combineReducers({
+    songs: songsReducer,
+    selectedSong: selectedSongReducer
+})
